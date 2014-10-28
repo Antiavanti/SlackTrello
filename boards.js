610bb8655ec568a35af6c991fb27c69e9e6bff7b
Wojciech Stachowski <wojciech.stachowski@gmail.com>
2014-10-28 10:46:04 +0100

    Update boards list

--- boards.js
+++ boards.js
@@ -1,6 +1,10 @@
 var trello = [
 	'Jw0MCG81/innovativeproject-ciherd',
 	'ZzWgZtvO/innovativeproject-resttest',
+	'is2oSaFq/innovativeproject-docdb',
+	'KigpoKUl/innovativeproject-whereisboss',
+	'QOFCsUyp/innovativeproject-lighci',
+	'A3a9CnxO/innovativeproject-lostsignal'
 ];
 
 var boards = trello.map(function(element) {
